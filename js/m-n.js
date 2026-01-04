d07f0ddca000983d23ec6a57984650d612b35993
5.1 <15012839+liu-zu-takes-off@user.noreply.gitee.com>
2026-01-04 16:36:05 +0800

    1

--- js/m-n.js
+++ js/m-n.js
@@ -1,15 +1,17 @@
- new Vue({
-      el: '#app',
-      data: {
-        activeMessageType: '私信',
-        activeConversation: '张三'
-      },
-      methods: {
-        selectMessageType(type) {
-          this.activeMessageType = type;
-        },
-        selectConversation(name) {
-          this.activeConversation = name;
-        }
-      }
-    });
+new Vue({
+  el: '#app',
+  data: {
+    activeMessageType: '私信',
+    activeConversation: '张三'
+  },
+  methods: {
+    // 选择消息类型
+    selectMessageType(type) {
+      this.activeMessageType = type;
+    },
+    // 选择会话
+    selectConversation(name) {
+      this.activeConversation = name;
+    }
+  }
+});
