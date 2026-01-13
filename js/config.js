cdbef85d1f2152fa4c16ae50a21e365be4673d22
5.1 <15012839+liu-zu-takes-off@user.noreply.gitee.com>
2026-01-13 11:55:59 +0800

    1

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const AppConfig = {
     // 如果是HTTPS页面，用8443端口
     if (window.location.protocol === 'https:') {
       // return 'wss://10.11.192.98:8443/StuForum_war/chat';
-      return 'wss://ws.lztflioveqzs.dpdns.org/StuForum_war/chat/';
+      return 'wss://ws.lztflioveqzs.dpdns.org/StuForum_war/chat';
     }
     // 否则用8080端口
     return 'ws://10.11.192.98:8080/StuForum_war/chat';
